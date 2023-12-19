0b4cbc1cb9941f31170da229edd495af60bcba65
root <iseki>
2023-12-19 23:57:35 +0900

    code format

--- Unity3D/Scripts/Underwater_u5.cs
+++ Unity3D/Scripts/Underwater_u5.cs
@@ -13,19 +13,19 @@ public class Underwater_u5 : MonoBehaviour
     private bool defaultFog;
     private Color defaultFogColor;
     private float defaultFogDensity;
-	private Material defaultSkybox;
+    private Material defaultSkybox;
 
     void Start () 
     {
-	    //Set the background color
+        //Set the background color
         defaultFog = RenderSettings.fog;
         defaultFogColor = RenderSettings.fogColor;
         defaultFogDensity = RenderSettings.fogDensity;
-		defaultSkybox = RenderSettings.skybox;
-		GetComponent<Camera>().backgroundColor = new Color(0, 0.4f, 0.7f, 1);
+        defaultSkybox = RenderSettings.skybox;
+        GetComponent<Camera>().backgroundColor = new Color(0, 0.4f, 0.7f, 1);
     }
  
-	//
+    //
     void Update () 
     {
         if (transform.position.y < underwaterLevel)
@@ -33,14 +33,14 @@ public class Underwater_u5 : MonoBehaviour
             RenderSettings.fog = true;
             RenderSettings.fogColor = new Color(0, 0.4f, 0.7f, 0.6f);
             RenderSettings.fogDensity = waterDensity;
-			RenderSettings.skybox = null;
+            RenderSettings.skybox = null;
         }
         else
         {
             RenderSettings.fog = defaultFog;
             RenderSettings.fogColor = defaultFogColor;
             RenderSettings.fogDensity = defaultFogDensity;
-			RenderSettings.skybox = defaultSkybox;
+            RenderSettings.skybox = defaultSkybox;
         }
     }
 }
