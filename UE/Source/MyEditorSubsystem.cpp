1827462d1fe90e33f26e782e5b4f71bd39ab6b89
fumi.hax <iseki@rsch.tuis.ac.jp>
2024-02-16 12:21:27 +0900

    for UE

--- UE/Source/MyEditorSubsystem.cpp
+++ UE/Source/MyEditorSubsystem.cpp
@@ -1,5 +1,6 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
+/**
+* Import Post処理 for OAR   by Fumi.Iseki
+**/
 
 #include "MyEditorSubsystem.h"
 #include "Subsystems/EditorAssetSubsystem.h"
@@ -12,298 +13,106 @@ void UMyEditorSubsystem::Initialize(FSubsystemCollectionBase& Collection)
 	if (GEditor == NULL) return;
 	UImportSubsystem* ImportSubsystem = GEditor->GetEditorSubsystem<UImportSubsystem>();
 
-	if (ImportSubsystem != NULL)
-	{
+	if (ImportSubsystem != NULL){
 		OnAssetPostImportHandle = ImportSubsystem->OnAssetPostImport.AddUObject(this, &UMyEditorSubsystem::OnAssetPostImport);
-		OnAssetPreImportHandle = ImportSubsystem->OnAssetPreImport.AddUObject(this, &UMyEditorSubsystem::OnAssetPreImport);
 	}
-}
-
-
-
-void UMyEditorSubsystem::OnAssetPostImport(UFactory* Factory, UObject* CreatedObject)
-{
-
-	if (CreatedObject != NULL) {
-		UClass* _uclass = CreatedObject->GetClass();
-		if (_uclass == NULL) return;
-
-		FString _class = _uclass->GetName();
-
-
-		if (_class.Equals(FString(TEXT("StaticMesh")))) {
-			UStaticMesh* mesh = Cast<UStaticMesh>(CreatedObject);
-
-			UE_LOG(LogTemp, Log, TEXT("Mesh Name = %s"), *(CreatedObject->GetName()));
-			
-			int i = 0;
-			auto* mtlif = mesh->GetMaterial(i);
-
-			while (mtlif != NULL) {
-				FString _path = mtlif->GetPathName();
-				FString _name = mtlif->GetName();
-				//
-				FString texture_path = GetTexturePath(_path, _name);
-				TArray<float> params = GetTextureParams(_name);
-
-				UE_LOG(LogTemp, Log, TEXT("Material Name = %d: %s (%f, %f, %f) %f"), i, *_name, 
-					params[0], params[1], params[2], params[23]);
-
-				UMaterialInstanceDynamic* material = UMaterialInstanceDynamic::Create(mtlif, NULL);
-				if (material == NULL) {
-					mtlif = mesh->GetMaterial(++i);
-					continue;
-				}
-
-				UTexture* texture = Cast<UTexture>(StaticLoadObject(UTexture::StaticClass(), NULL, *texture_path));
-				if (texture != NULL) {
-					material->SetTextureParameterValue(FName(TEXT("Texture")), texture);
-				}
-
-				material->SetVectorParameterValue(FName(TEXT("BaseColor")), FLinearColor(params[0], params[1], params[2]));
-				material->SetVectorParameterValue(FName(TEXT("BlendMode")), FLinearColor(params[0], params[1], params[2]));
-				material->SetScalarParameterValue(FName(TEXT("Shininess")), params[5]);
-				//material->Get
-
-
-				mesh->SetMaterial(i, material);
-				mtlif = mesh->GetMaterial(++i);
-
-
-
-
-
-			//CreatedObject->GetAssetRegistryTags(asset);
-
-			//for (auto& aaa : masset) {
-				//aaa.ToSt
-
-			
-			//TArray
-			//TArray<FStaticMaterial> smat = mesh->GetStaticMaterials();
-			//int i = 0;
-			//for (auto& mat : smat) {
-			//	//auto* matif = mesh->GetMaterial(i);
-			//	//FString mname = matif->GetName();
-			//
-			//	auto matif = mat.MaterialInterface;
-			//	FString mname = matif.GetName();
-			//	UE_LOG(LogTemp, Log, TEXT("material -=========> %d: %s"),i, *mname);
-			//
-			//	UMaterialInstanceDynamic* material = UMaterialInstanceDynamic::Create(matif, NULL);
-			//	material->SetVectorParameterValue(FName(TEXT("BaseColor")), FLinearColor(0.0f, 0.0f, 1.0f));
-			//	mesh->SetMaterial(i, material);
-			//	i++;
-			//	//delete &mat;
-			//}
-			//mesh->SetStaticMaterials(smat);
-			
-
-			
-
-
-				/*
-				TArray<FMaterialParameterInfo> pi;
-				TArray<FGuid> ids;
-				mtlif->GetAllTextureParameterInfo(pi, ids);
-				for (auto aaa : pi) {	
-					UE_LOG(LogTemp, Log, TEXT("Texture Param -=========> %s %s %d"), *(aaa.Name.ToString()), *(aaa.ToString()), aaa.Index);
-				}*/
-
-				/*
-				UTexture* text;
-				if (mtlif->GetTextureParameterValue(FName(TEXT("Texture")), text)) {
-					//mtlif->GetTextureParameterValue(FName(TEXT("map_Kd")), text);
-
-					// UTexture * texture = LoadObject<UTexture>（NULL, TEXT（"Floors.Stone"）, NULL, 0, NULL）;
-					UE_LOG(LogTemp, Log, TEXT("Texture Param -=========> %s"), *(text->GetName()));
-					UE_LOG(LogTemp, Log, TEXT("Texture FFF Param -=========> %s"), *(text->GetFullName()));
-				}
-
-				TArray<UTexture*> ptex;
-				mtlif->GetUsedTextures(ptex, EMaterialQualityLevel::Num, false, ERHIFeatureLevel::Num, true);
-				for (int32 TextureIndex = 0; TextureIndex < ptex.Num(); ++TextureIndex) {
-					UTexture2D* Texture = Cast<UTexture2D>(ptex[TextureIndex]);
-					UE_LOG(LogTemp, Log, TEXT("Texture2D Param -=========> %s %d %s"), *(Texture->GetName()), Texture->GetUniqueID()
-						, *(Texture->GetExternalTextureGuid().ToString()));
-				}*/
-
-				//UTexture* texture = LoadObject<UTexture>(NULL, TEXT("6b2665ac-f74f-4737-873e-e1861843c3ab.tga"), NULL, 0, NULL);
-				//UTexture* texture = NewObject<UTexture>(NULL, TEXT("6b2665ac-f74f-4737-873e-e1861843c3ab"), RF_Public | RF_Standalone);				
-			}
-
-			
-			
-			//UMaterialInterface* matif = mesh->GetMaterial(0);
-			//auto* matif = mesh->GetMaterial(0);
-			//FString mname = matif->GetName();
-			//UE_LOG(LogTemp, Log, TEXT("material -=========> %s"), *mname);
-
-			//UMaterialInstanceDynamic* material = UMaterialInstanceDynamic::Create(matif, NULL);
-
-			//material->SetVectorParameterValue(FName(TEXT("BaseColor")), FLinearColor(1.0f, 0.0f, 0.0f));
-			//material->SetVectorParameterValue(FName(TEXT("DiffuseColor")), FLinearColor(1.0f, 0.0f, 0.0f));
-			//material->SetVectorParameterValue(FName(TEXT("Color")), FLinearColor(1.0f, 0.0f, 0.0f));
-
-			//mesh->SetMaterial(0, material);
-		}
-
-
-
-
-/*
-		if (Factory != NULL) {
-			FName _name = Factory->GetFName();
-			UE_LOG(LogTemp, Log, TEXT("DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD> %s"), *_name.ToString());
-		}
-		*/
-		
-
-		/*
-		//UE_LOG(LogTemp, Log, TEXT("SSS> %s"), *_name);
-		if (_namexx.Find(FString(TEXT("MATERIAL_"))) == 0) {
-			UE_LOG(LogTemp, Log, TEXT("GGGGGGGGGGGGGGGGGGGGGGGG> %s"), *_namexx);
-			UE_LOG(LogTemp, Log, TEXT("class -=========> %s"), *_classxx);
-
-
-
-
-			//UEditorAssetSubsystem* EditorAssetSubsystem = GEditor->GetEditorSubsystem<UEditorAssetSubsystem>();
-
-
-
-			//UMaterialInstanceDynamic* material = (UMaterialInstanceDynamic*)CreatedObject;
-			//UMaterialInstanceDynamic* material = UMaterialInstanceDynamic::Create(NULL, CreatedObject);
-			//CreatedObject = ma
-			//material->SetVectorParameterValue(FName(TEXT("BaseColor")), FLinearColor(1.0f, 0.0f, 0.0f));
-			//UMaterialInstance* material = (UMaterialInstance*)CreatedObject;
-			//CreatedObject = *material;
-			//UMaterialInstance* material = (UMaterialInstance*)CreatedObject;
-
-			//EditorAssetSubsystem->SetMetadataTag(CreatedObject, FName(TEXT("diffuse_color")), TEXT("{R:1.0, G:0.0, B:0.0}"));
-			//EditorAssetSubsystem->SetMetadataTag(CreatedObject, FName(TEXT("base_color")), TEXT("{R:1.0, G:0.0, B:0.0}"));
-			//EditorAssetSubsystem->SetMetadataTag(CreatedObject, FName(TEXT("BaseColor")), TEXT("{R:1.0, G:0.0, B:0.0}"));
-			//EditorAssetSubsystem->SetMetadataTag(CreatedObject, FName(TEXT("DiffuseColor")), TEXT("{R:1.0, G:0.0, B:0.0}"));
-
-			//FString _tag;
-			//_tag = EditorAssetSubsystem->GetMetadataTag(CreatedObject, "BaseColor");
-			//UE_LOG(LogTemp, Log, TEXT("TTTTTTTTTTTTTTTTTTTTTTTTT> %s"), *_tag);
-			//_tag = EditorAssetSubsystem->GetMetadataTag(CreatedObject, "Color");
-			//UE_LOG(LogTemp, Log, TEXT("TTTTTTTTTTTTTTTTTTTTTTTTT> %s"), *_tag);
-			//_tag = EditorAssetSubsystem->GetMetadataTag(CreatedObject, "DiffuseColor");
-			//UE_LOG(LogTemp, Log, TEXT("TTTTTTTTTTTTTTTTTTTTTTTTT> %s"), *_tag);
-			//_tag = EditorAssetSubsystem->GetMetadataTag(CreatedObject, "base_color");
-			//UE_LOG(LogTemp, Log, TEXT("TTTTTTTTTTTTTTTTTTTTTTTTT> %s"), *_tag);
-			//_tag = EditorAssetSubsystem->GetMetadataTag(CreatedObject, "diffuse_color");
-			//UE_LOG(LogTemp, Log, TEXT("TTTTTTTTTTTTTTTTTTTTTTTTT> %s"), *_tag);
-
-
-			//CreatedObject->Modify();
-
-			//UMaterial* material = (UMaterial*)CreatedObject;
-			//material->Set
-
-
-			//FString _class = CreatedObject->GetClass()->GetName(); // Material
-			//UE_LOG(LogTemp, Log, TEXT("class -=========> %s"), *_class);
-			//material->
-
-			//FMaterialCompiler* xxxx = new FMaterialCompiler();
-			UMaterialInstance* xxx = Cast<UMaterialInstance>(CreatedObject);
-
-			UMaterialInstanceDynamic* material = UMaterialInstanceDynamic::Create(xxx, NULL);
-			material->SetVectorParameterValue(FName(TEXT("BaseColor")), FLinearColor(1.0f, 0.0f, 0.0f));
-
-
-			//FName _sname = CreatedObject->SourceFileTagName();
-			//FAssetData asset;
-			//TArray<UObject::FAssetRegistryTag> masset;
-			//CreatedObject->GetAssetRegistryTags(asset);
-
-			//for (auto& aaa : masset) {
-				//aaa.ToSt
-
-			//CreatedObject->Ass
-			//UE_LOG(LogTemp, Log, TEXT("====================> %s"), *(masset.ToString()));
-
-
-
-			//asset.key
-
-			//UE_LOG(LogTemp, Log, TEXT("====================> %s"), *_sname.ToString());
-
-
-
-			//FAssetData
-			//CreatedObject->GetMate
-			//UMaterialInstanceDynamic* material = UMaterialInstanceDynamic::Create(NULL, CreatedObject);
-
-
-
-
-			//material->Modify();
-
-			//material->SetVectorParameterValue(FName(TEXT("Base Color")), FLinearColor(1.0f, 0.0f, 0.0f));
-
-			//CreatedObject->Modify();
-
-
-			//EditorAssetSubsystem->SetMetadataTag(material, FName(TEXT("diffuse_color")), TEXT("{\"R\":1.0, \"G\":0.0, \"B\":0.0}"));
-			//EditorAssetSubsystem->SetMetadataTag(material, FName(TEXT("Input DiffuseColor")), TEXT("{\"R\":1.0, \"G\":0.0, \"B\":0.0}"));
-			//EditorAssetSubsystem->SetMetadataTag(material, FName(TEXT("Input DiffuseColor (Vector3)")), TEXT("{\"R\":1.0, \"G\":0.0, \"B\":0.0}"));
-			//EditorAssetSubsystem->SetMetadataTag(material, FName(TEXT("base_color")), TEXT("{\"R\":1.0, \"G\":0.0, \"B\":0.0}"));
-			//EditorAssetSubsystem->SetMetadataTag(material, FName(TEXT("BaseColor")), TEXT("{\"R\":1.0, \"G\":0.0, \"B\":0.0}"));
-
-			//TMap<FName, FString> _maps = EditorAssetSubsystem->GetMetadataTagValues(material);
-			//for (auto& aaa : _maps) {
-			//	UE_LOG(LogTemp, Log, TEXT("xxxxxxxxxxxxxxx> %s  %s"), *(aaa.Key).ToString(), *aaa.Value);
-			//}
-		}*/
-	}
-	//UE_LOG(LogTemp, Log, TEXT("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"));
-
-}
-
-
-
-void UMyEditorSubsystem::OnAssetPreImport(UFactory* Factory, UClass* InClass, UObject* CreatedObject, const FName& Name, const TCHAR* Type)
-{
-	return;
-	/**/
-	UE_LOG(LogTemp, Log, TEXT("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX name %s"), *Name.ToString());
-	UE_LOG(LogTemp, Log, TEXT("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX type %s"), Type);
-
-	if (Factory != NULL) {
-		FString _class = Factory->GetClass()->GetName();
-		UE_LOG(LogTemp, Log, TEXT("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX Factory %s"), *_class);
-	}
-
-
-	if (CreatedObject != NULL) {
-		FString _class = CreatedObject->GetClass()->GetName();
-		UE_LOG(LogTemp, Log, TEXT("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX Object %s"), *_class);
-	}
-
-	if (InClass != NULL) {
-		FString _class = InClass->GetClass()->GetName();
-		UE_LOG(LogTemp, Log, TEXT("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX class %s"), *_class);
-	}
-	/**/
 }
 
 
 void UMyEditorSubsystem::Deinitialize()
 {
-	if (GEditor != NULL)
-	{
+	if (GEditor != NULL) {
 		UImportSubsystem* ImportSubsystem = GEditor->GetEditorSubsystem<UImportSubsystem>();
-		if (ImportSubsystem != NULL)
-		{
+		if (ImportSubsystem != NULL) {
 			ImportSubsystem->OnAssetPostImport.Remove(OnAssetPostImportHandle);
-			ImportSubsystem->OnAssetPreImport.Remove(OnAssetPreImportHandle);
 		}
 	}
 	Super::Deinitialize();
+}
+
+
+void UMyEditorSubsystem::OnAssetPostImport(UFactory* Factory, UObject* CreatedObject)
+{
+	if (CreatedObject != NULL) {
+		FString _class_name = CreatedObject->GetClass()->GetName();
+
+		// Mesh Object
+		if (_class_name.Equals(FString(TEXT("StaticMesh")))) {
+			UStaticMesh* mesh = Cast<UStaticMesh>(CreatedObject);
+			
+			int i = 0;
+			auto* mtlif = mesh->GetMaterial(i);
+			while (mtlif != NULL) {
+				FString _name = mtlif->GetName();
+				//
+				TArray<float> params = GetTextureParams(_name);
+				if (params[23] == 0.0f) {
+					mtlif = mesh->GetMaterial(++i);
+					continue;
+				}
+
+				UMaterialInterface* new_mtlif = SelectMaterialInterface(params);
+				if (new_mtlif == NULL) {
+					mtlif = mesh->GetMaterial(++i);
+					continue;
+				}
+
+				// Setup Parameters
+				UMaterialInstanceDynamic* material = UMaterialInstanceDynamic::Create(new_mtlif, NULL);
+				if (material != NULL) {
+					// Texture
+					FString _path = mtlif->GetPathName();
+					FString texture_path = GetTexturePath(_path, _name);
+					UTexture* texture = Cast<UTexture>(StaticLoadObject(UTexture::StaticClass(), NULL, *texture_path));
+					if (texture != NULL) {
+						material->SetTextureParameterValue(FName(TEXT("Texture")), texture);
+					}
+					// Some Parameters
+					material->SetVectorParameterValue(FName(TEXT("BaseColor")), FLinearColor(params[0], params[1], params[2]));
+					material->SetScalarParameterValue(FName(TEXT("Transparent")), params[3]);
+					material->SetScalarParameterValue(FName(TEXT("Cutoff")), params[4]);
+					material->SetScalarParameterValue(FName(TEXT("Shininess")), params[5]);
+					material->SetScalarParameterValue(FName(TEXT("Glow")), params[6]);
+					material->SetScalarParameterValue(FName(TEXT("Bright")), params[7]);
+					material->SetScalarParameterValue(FName(TEXT("Light")), params[8]);
+					// Setup
+					mesh->SetMaterial(i, material);
+				}
+				//
+				mtlif = mesh->GetMaterial(++i);
+			}
+		}
+	}
+}
+
+
+UMaterialInterface* UMyEditorSubsystem::SelectMaterialInterface(TArray<float> params)
+{
+	UMaterialInterface* mtlif = NULL;
+
+	UE_LOG(LogTemp, Log, TEXT("Material Params = (%f, %f)"), params[4], params[5]);
+
+	int kind = (int)(params[23] + 0.5f);
+	if (kind == 84 || kind == 71) {		// 'T' or 'G'
+		mtlif = Cast<UMaterialInterface>(StaticLoadObject(UMaterialInterface::StaticClass(), NULL,
+			*FString(TEXT(MATERIAL_SHADER_ALPHA))));
+	}
+	else if (kind == 79) {				// 'O'
+		if (params[3] < 0.99f && params[4] < 0.01f) {	 // Transparent & Cutoff
+			mtlif = Cast<UMaterialInterface>(StaticLoadObject(UMaterialInterface::StaticClass(), NULL,
+				*FString(TEXT(MATERIAL_SHADER_ALPHA))));
+		}
+		else {
+			mtlif = Cast<UMaterialInterface>(StaticLoadObject(UMaterialInterface::StaticClass(), NULL,
+				*FString(TEXT(MATERIAL_SHADER_OBJ))));
+		}
+	}
+	else if (kind == 69) {				// 'E'
+		mtlif = Cast<UMaterialInterface>(StaticLoadObject(UMaterialInterface::StaticClass(), NULL,
+			*FString(TEXT(MATERIAL_SHADER_OBJ))));
+	}
+
+	return mtlif;
 }
 
 
@@ -328,28 +137,30 @@ FString UMyEditorSubsystem::GetTexturePath(FString mtl_path, FString mtl_name)
 TArray<float> UMyEditorSubsystem::GetTextureParams(FString mtl_name)
 {
 	TArray<float> params;
-	params.Init(0.0, 24);
-	params[3] = 1.0f;
+	params.Init(0.0, MATERIAL_PARAMS_SIZE);
+	params[3] = 1.0f;	// Transparent
 
-	int32 _end_name = mtl_name.Find(TEXT("_"), ESearchCase::CaseSensitive, ESearchDir::FromEnd, mtl_name.Len());
-	FString _params_str = mtl_name.Right(mtl_name.Len() - _end_name - 1);
+	int32 _name_end = mtl_name.Find(TEXT("_"), ESearchCase::CaseSensitive, ESearchDir::FromEnd, mtl_name.Len());
+	FString _params_str = mtl_name.Right(mtl_name.Len() - _name_end - 1);
+
+	if (_params_str.Len() != MATERIAL_BASE64_SIZE) return params;
 	_params_str = _params_str.Replace(TEXT("-"), TEXT("/"));
-	UE_LOG(LogTemp, Log, TEXT("params = %s"),*_params_str);
+	//UE_LOG(LogTemp, Log, TEXT("params = %s"), *_params_str);
 
 	TArray<uint8> dec;
 	FBase64::Decode(_params_str, dec);
-	if (dec.Num() != 24) return params;
+	if (dec.Num() != MATERIAL_PARAMS_SIZE) return params;
 
-	params[0]  = 1.0f - (float)dec[0] / 255.0f;		// Red
-	params[1]  = 1.0f - (float)dec[1] / 255.0f;		// Green
-	params[2]  = 1.0f - (float)dec[2] / 255.0f;		// Blue
-	params[3]  = 1.0f - (float)dec[3] / 255.0f;		// Alpha
-	params[4]  = (float)dec[4] / 255.0f;			// CutOff
-	params[5]  = (float)dec[5] / 255.0f;			// Shinuness
-	params[6]  = (float)dec[6] / 255.0f;			// Glow
-	params[7]  = (float)dec[7] / 255.0f;			// Bright
-	params[8]  = (float)dec[8] / 255.0f;			// Light
-	params[23] = (float)dec[23];					// Kind
+	params[0]  = 1.0f - (float)dec[0] / 255.0f;			// Red
+	params[1]  = 1.0f - (float)dec[1] / 255.0f;			// Green
+	params[2]  = 1.0f - (float)dec[2] / 255.0f;			// Blue
+	params[3]  = 1.0f - (float)dec[3] / 255.0f;			// Alpha
+	params[4]  = (float)dec[4] / 255.0f;				// Cutoff
+	params[5]  = (float)dec[5] / 255.0f;				// Shinuness
+	params[6]  = (float)dec[6] / 255.0f;				// Glow
+	params[7]  = (float)dec[7] / 255.0f;				// Bright
+	params[8]  = (float)dec[8] / 255.0f;				// Light
+	params[23] = (float)dec[MATERIAL_PARAMS_SIZE - 1];	// Kind
 
 	return params;
 }
--- UE/Source/MyEditorSubsystem.h
+++ UE/Source/MyEditorSubsystem.h
@@ -1,36 +1,39 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+/**
+* Import Postèàóù for OAR   by Fumi.Iseki
+*
+*          ver1.0b  2024/02/16
+**/
 
 #pragma once
+
+#define  MATERIAL_SHADER_OBJ    "/Game/Material/OpenSim_OBJ"
+#define  MATERIAL_SHADER_ALPHA  "/Game/Material/OpenSim_Alpha"
+#define  MATERIAL_PARAMS_SIZE   24
+#define  MATERIAL_BASE64_SIZE   32
+
 
 #include "CoreMinimal.h"
 #include "EditorSubsystem.h"
 #include "MyEditorSubsystem.generated.h"
 
+
 /**
  * 
  */
 UCLASS()
-class MYPROJECT2_API UMyEditorSubsystem : public UEditorSubsystem
+class MYPROJECT_API UMyEditorSubsystem : public UEditorSubsystem
 {
 	GENERATED_BODY()
 	
-
 public:
-	// Begin UEditorSubsystem Interface
 	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
 	virtual void Deinitialize() override;
-	// End UEditorSubsystem Interface
-
 
 	FDelegateHandle OnAssetPostImportHandle;
-	FDelegateHandle OnAssetPreImportHandle;
-
-	//UPROPERTY(Transient)
 	void OnAssetPostImport(UFactory* Factory, UObject* CreatedObject);
-	void OnAssetPreImport(UFactory* InFactory, UClass* InClass, UObject* InParent, const FName& Name, const TCHAR* Type);
 
+	UMaterialInterface* SelectMaterialInterface(TArray<float> params);
 
 	FString GetTexturePath(FString mtl_path, FString mtl_name);
-
 	TArray<float> GetTextureParams(FString mtl_name);
 };
