ecf3769eea54cdda859e525108f5652d78401e89
root <iseki>
2024-05-18 19:28:00 +0900

    joints b

--- OARTool.cpp
+++ OARTool.cpp
@@ -904,8 +904,8 @@ void*  OARTool::generateSolidData(int format, const char* fname, int num, bool u
         // DAE
         if (format==JBXL_3D_FORMAT_DAE) {
             if (count==1 && forUnity4) dae->addCenterObject();          // for Unity4.x
-            dae->getObjectCenter();
-            dae->setJointLocationMatrix();
+            //dae->getObjectCenter();
+            //dae->setJointLocationMatrix();
             return (void*)dae;
         }
         //  OBJ
