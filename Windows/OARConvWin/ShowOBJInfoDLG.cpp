e5eddfe468ab3bde4495fd28fbc50c57380e3fc2
fumi.iseki <iseki@rsch.tuis.ac.jp>
2024-05-22 10:30:41 +0900

    Add ShowOBJInfoDLG

---
+++ Windows/OARConvWin/ShowOBJInfoDLG.cpp
@@ -0,0 +1,77 @@
+﻿// ShowOBJInformationDLG.cpp : 実装ファイル
+//
+
+#include "stdafx.h"
+#include "OARConvWin.h"
+#include "ShowOBJInfoDLG.h"
+
+
+// CShowInfoDLG ダイアログ
+
+IMPLEMENT_DYNAMIC(CShowOBJInfoDLG, CDialogEx)
+
+
+CShowOBJInfoDLG::CShowOBJInfoDLG(OARTool oar, CWnd* pParent)
+	: CDialogEx(CShowOBJInfoDLG::IDD, pParent)
+{
+	/*
+	objectName;
+	coordinate;
+	other;
+	coordVec;
+	vertexNnum;
+	jointNum;
+	*/
+
+	nameSBox   = NULL;
+	coordSBox  = NULL;
+	vertexSBox = NULL;
+	jointSBox  = NULL;
+	otherSBox  = NULL;
+}
+
+
+
+CShowOBJInfoDLG::~CShowOBJInfoDLG()
+{
+}
+
+
+
+void CShowOBJInfoDLG::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+
+BEGIN_MESSAGE_MAP(CShowOBJInfoDLG, CDialogEx)
+END_MESSAGE_MAP()
+
+
+
+////////////////////////////////////////////////////////////////////////
+// CShowInfoDLG メッセージ ハンドラー
+
+BOOL   CShowOBJInfoDLG::OnInitDialog()
+{
+	//TCHAR buf[LNAME];
+
+	nameSBox   = (CStatic*)GetDlgItem(IDC_STATIC_OBJNAME);
+	coordSBox  = (CStatic*)GetDlgItem(IDC_STATIC_CNTCRD);
+	vertexSBox = (CStatic*)GetDlgItem(IDC_STATIC_VRTXNUM);
+	jointSBox  = (CStatic*)GetDlgItem(IDC_STATIC_JNTNUM);
+	otherSBox  = (CStatic*)GetDlgItem(IDC_STATIC_OTHER);
+
+	/*
+	sntprintf(buf, LNAME, _T("%s"), regionName.GetString());
+	nameSBox->SetWindowText(buf);
+	sntprintf(buf, LNAME, _T("%d.%d"), majorVersion, minorVersion);
+	verSBox->SetWindowText(buf);
+	sntprintf(buf, LNAME, _T("%dx%d"), rgSize, rgSize);
+	sizeSBox->SetWindowText(buf);
+	sntprintf(buf, LNAME, _T("%d"), objNum);
+	numSBox->SetWindowText(buf);
+	*/
+	return TRUE;
+}
---
+++ Windows/OARConvWin/ShowOBJInfoDLG.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+
+
+// CShowOBJInfoDLG ダイアログ
+
+class CShowOBJInfoDLG : public CDialogEx
+{
+	DECLARE_DYNAMIC(CShowOBJInfoDLG)
+
+public:
+	CString objectName;
+	CString coordinate;
+	CString other;
+	Vector<double> coordVec;
+	int    vertexNnum;
+	int	   jointNum;
+
+	CStatic* nameSBox;
+	CStatic* coordSBox;
+	CStatic* vertexSBox;
+	CStatic* jointSBox;
+	CStatic* otherSBox;
+
+public:
+	CShowOBJInfoDLG(OARTool oar, CWnd* pParent=NULL);   // 標準コンストラクター
+	virtual ~CShowOBJInfoDLG();
+
+	virtual BOOL  OnInitDialog();
+
+// ダイアログ データ
+	enum { IDD = IDD_DIALOG_OARINFO };
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV サポート
+
+	DECLARE_MESSAGE_MAP()
+};
