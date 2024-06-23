0819039a2af62d94f44a5e3709e44636316e8ad6
fumihax <iseki@rsch.tuis.ac.jp>
2024-06-23 21:07:51 +0900

    Up UE plugins

--- UE/Plugins/OARSelectMaterial/Source/OARSelectMaterial/Public/OAREditorSubsystem.h
+++ UE/Plugins/OARSelectMaterial/Source/OARSelectMaterial/Public/OAREditorSubsystem.h
@@ -6,16 +6,18 @@
 
 #pragma once
 
-#define  MATERIAL_BASE64_SIZE   32
-#define  MATERIAL_PARAMS_SIZE   24
-#define  MATERIAL_PARAMS_KIND   (MATERIAL_PARAMS_SIZE - 1)
+#define  MATERIAL_BASE64_SIZE       32
+#define  MATERIAL_PARAMS_SIZE       24
+#define  MATERIAL_PARAMS_KIND       (MATERIAL_PARAMS_SIZE - 1)
 
-#define  MATERIAL_SHADER_OBJ    "/OARSelectMaterial/Material/OAR_OBJ"
-#define  MATERIAL_SHADER_MASK   "/OARSelectMaterial/Material/OAR_Mask"
-#define  MATERIAL_SHADER_ALPHA  "/OARSelectMaterial/Material/OAR_Alpha"
+#define  MATERIAL_SHADER_TREE       "/OARSelectMaterial/Material/OAR_Tree"
+#define  MATERIAL_SHADER_EARTH      "/OARSelectMaterial/Material/OAR_Earth"
+#define  MATERIAL_SHADER_GENERAL    "/OARSelectMaterial/Material/OAR_General"
+#define  MATERIAL_SHADER_TRANS      "/OARSelectMaterial/Material/OAR_Trans"
+#define  MATERIAL_SHADER_CUTOFF     "/OARSelectMaterial/Material/OAR_Trans"
 
-#define  OBJ_PHANTOM_PREFIX     "UNC_"
-#define  OBJ_COLLIDER_PREFIX    "UCP_"
+#define  OBJ_PHANTOM_PREFIX         "UNC_"
+#define  OBJ_COLLIDER_PREFIX        "UCP_"
 
 
 #include "CoreMinimal.h"
@@ -34,6 +36,25 @@ class OARSELECTMATERIAL_API UOAREditorSubsystem : public UEditorSubsystem
 {
 	GENERATED_BODY()
 
+private:
+    float colorRed      = 0.0f;
+    float colorGreen    = 0.0f;
+    float colorBlue     = 0.0f;
+    float transparent   = 1.0f;  //
+    float cutoff        = 0.0f;
+    float shininess     = 0.0f;
+    float glow          = 0.0f;
+    float bright        = 0.0f;
+    float light         = 0.0f;
+    int   alphaMode     = 0;     // 0: NONE, 1: BLENDING, 2: MASKING, 3: EMISSIVE
+    bool  hasAlpha      = false;
+//  float shiftU        = 0.0f;
+//  float shiftV        = 0.0f;
+//  float scaleU        = 1.0f;
+//  float scaleV        = 1.0f;
+//  float rotate        = 0.0f;
+    char  kind          = 'O';   // O: Object, T: Tree, G: Grass, E: Earth
+
 public:
 	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
 	virtual void Deinitialize() override;
@@ -41,8 +62,8 @@ public:
 	FDelegateHandle OnAssetPostImportHandle;
 	void OnAssetPostImport(UFactory* Factory, UObject* CreatedObject);
 
-	UMaterialInterface* SelectMaterialInterface(TArray<float> params);
+	UMaterialInterface* SelectMaterialInterface();
 
 	FString GetTexturePath(FString mtl_path, FString mtl_name);
-	TArray<float> GetTextureParams(FString mtl_name);
+	void    GetTextureParams(FString mtl_name);
 };
